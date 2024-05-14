4660f26e28974f9c248c9033f3e74d9d4ce8f876
Akseenja <klasik1995@bk.ru>
2024-05-14 18:24:56 +0300

    initial changes

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -2,19 +2,124 @@ import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
 
 import styles from './ArticleParamsForm.module.scss';
+import { FormEvent, useState } from 'react';
+import clsx from 'clsx';
 
-export const ArticleParamsForm = () => {
+import {
+	ArticleStateType,
+	backgroundColors,
+	contentWidthArr,
+	defaultArticleState,
+	fontColors,
+	fontFamilyOptions,
+	fontSizeOptions,
+} from 'src/constants/articleProps';
+import { Select } from '../select';
+import { RadioGroup } from '../radio-group';
+import { Separator } from '../separator';
+import { Text } from '../text';
+
+type ParamStateFromProps = {
+	setCurrentParamsState: (articleState: ArticleStateType) => void;
+};
+
+const defaultFontFamily = defaultArticleState.fontFamilyOption;
+const defaultFontSize = defaultArticleState.fontSizeOption;
+const defaultFontColor = defaultArticleState.fontColor;
+const defaultBackgroundColor = defaultArticleState.backgroundColor;
+const defaultcontentWidth = defaultArticleState.contentWidth;
+
+export const ArticleParamsForm = ({
+	setCurrentParamsState,
+}: ParamStateFromProps) => {
+	const setParamsToDefault = () => {
+		setFontFamily(defaultFontFamily);
+		setFontSize(defaultFontSize);
+		setFontColor(defaultFontColor);
+		setBackgroundColor(defaultBackgroundColor);
+		setContentWidth(defaultcontentWidth);
+	};
+
+	const [isOpen, setIsOpen] = useState<boolean>(false);
+
+	const [fontFamily, setFontFamily] = useState(defaultFontFamily);
+	const [fontSize, setFontSize] = useState(defaultFontSize);
+	const [fontColor, setFontColor] = useState(defaultFontColor);
+	const [backgroundColor, setBackgroundColor] = useState(
+		defaultBackgroundColor
+	);
+	const [contentWidth, setContentWidth] = useState(defaultcontentWidth);
+
+	const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		console.log('change: submit form');
+		const currentParamsData = {
+			fontFamilyOption: fontFamily,
+			fontSizeOption: fontSize,
+			fontColor: fontColor,
+			backgroundColor: backgroundColor,
+			contentWidth: contentWidth,
+		};
+		setCurrentParamsState(currentParamsData);
+	};
+
+	const handleReset = () => {
+		setCurrentParamsState(defaultArticleState);
+		setParamsToDefault();
+	};
+
 	return (
 		<>
-			<ArrowButton />
-			<aside className={styles.container}>
-				<form className={styles.form}>
-					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' type='reset' />
-						<Button title='Применить' type='submit' />
-					</div>
-				</form>
-			</aside>
+			<ArrowButton isOpen={isOpen} setIsOpen={() => setIsOpen(!isOpen)} />
+			{isOpen && (
+				<aside className={clsx(styles.container, styles.container_open)}>
+					<form
+						className={styles.form}
+						onSubmit={handleSubmit}
+						onReset={handleReset}>
+						<Text weight={800} size={31} uppercase={true}>
+							Задайте параметры
+						</Text>
+
+						<Select
+							title={'Шрифт'}
+							selected={fontFamily}
+							options={fontFamilyOptions}
+							onChange={setFontFamily}></Select>
+
+						<RadioGroup
+							title='Размер шрифта'
+							selected={fontSize}
+							options={fontSizeOptions}
+							onChange={setFontSize}
+							name='fontSize'></RadioGroup>
+
+						<Select
+							title='Цвет шрифта'
+							selected={fontColor}
+							options={fontColors}
+							onChange={setFontColor}></Select>
+
+						<Separator></Separator>
+
+						<Select
+							title='Цвет фона'
+							selected={backgroundColor}
+							options={backgroundColors}
+							onChange={setBackgroundColor}></Select>
+						<Select
+							title='Ширина контента'
+							selected={contentWidth}
+							options={contentWidthArr}
+							onChange={setContentWidth}></Select>
+
+						<div className={styles.bottomContainer}>
+							<Button title='Сбросить' type='reset' />
+							<Button title='Применить' type='submit' />
+						</div>
+					</form>
+				</aside>
+			)}
 		</>
 	);
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties } from 'react';
+import { StrictMode, CSSProperties, useState } from 'react';
 import clsx from 'clsx';
 
 import { Article } from './components/article/Article';
@@ -13,19 +13,22 @@ const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
 const App = () => {
+	const [currentParamsState, setCurrentParamsState] =
+		useState(defaultArticleState);
+
 	return (
 		<div
 			className={clsx(styles.main)}
 			style={
 				{
-					'--font-family': defaultArticleState.fontFamilyOption.value,
-					'--font-size': defaultArticleState.fontSizeOption.value,
-					'--font-color': defaultArticleState.fontColor.value,
-					'--container-width': defaultArticleState.contentWidth.value,
-					'--bg-color': defaultArticleState.backgroundColor.value,
+					'--font-family': currentParamsState.fontFamilyOption.value,
+					'--font-size': currentParamsState.fontSizeOption.value,
+					'--font-color': currentParamsState.fontColor.value,
+					'--container-width': currentParamsState.contentWidth.value,
+					'--bg-color': currentParamsState.backgroundColor.value,
 				} as CSSProperties
 			}>
-			<ArticleParamsForm />
+			<ArticleParamsForm setCurrentParamsState={setCurrentParamsState} />
 			<Article />
 		</div>
 	);
